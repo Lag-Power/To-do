746df94566efb86432266751c84f8172df8b89ff
Lag-Power <58886402+Lag-Power@users.noreply.github.com>
2023-04-03 11:22:41 -0500

    update web.py

--- web.py
+++ web.py
@@ -3,14 +3,10 @@ import functions
 
 todos = functions.get_todos()
 
-
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
-
-
-todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
@@ -21,7 +17,7 @@ for index, todo in enumerate(todos):
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todos]
+        del st.session_state[todo]
         st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
